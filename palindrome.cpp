c157f4e88d1471d4d31a681a23406b43e0aeb5e7
raedar03 <rehan010806@gmail.com>
2026-08-19 20:26:25 +0530

    adding comments to palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
+//To check whether an integer is palindrome. 
+#include <iostream> 
 using namespace std;
 int main() {
-    int UserNum, RemNum = 0, RevNum, TempNum;
+    int UserNum, RemNum = 0, RevNum, TempNum; //declaring variables.
     cout << "Enter A Number: ";
     cin >> UserNum;
     TempNum = UserNum;
     cout << "--------------------------------" <<endl;
     cout << "Your Number is: " << UserNum << endl;
-    while (TempNum != 0) {
-    	RemNum = TempNum%10;
-    	RevNum = (RevNum*10)+RemNum;
-    	TempNum = TempNum/10;
+    while (TempNum != 0) { //loop for reversing the number.
+    	RemNum = TempNum%10; //takes the last digit.
+    	RevNum = (RevNum*10)+RemNum; //adds digit in reverse order.
+    	TempNum = TempNum/10; //deletes the last digit of the original number (stored in temporary variable).
 	}
 	cout << "Reversed Number is: " << UserNum << endl;
-	if (UserNum==RevNum) {
+	if (UserNum==RevNum) { //condition to check if orginal number is equal to reversed number.
 		cout << "Palindrome!";
 	} else {
 		cout << "Not Palindrome!";
 	}
     return 0;
-}
+} //end of program.
 
 
